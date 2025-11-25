1a4b1e7fad714e341526bf46aba3d8b8c8af6296
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-25 12:57:31 +0500

    Add queries for messages

--- db/queries.js
+++ db/queries.js
@@ -20,8 +20,29 @@ async function findUserById(id) {
   return result.rows[0];
 }
 
+async function getAllMessages() {
+  const result = await pool.query(`
+    SELECT messages.*, users.username
+    FROM messages
+    JOIN users ON messages.user_id = users.id
+    ORDER BY created_at DESC`);
+  return result.rows;
+}
+
+async function createMessage(userid, title, body) {
+  const result = await pool.query(
+    `
+    INSERT INTO messages (user_id, title, body, created_at)
+    VALUES ($1, $2, $3, NOW() RETURNING *)`,
+    [userid, title, body]
+  );
+  return result.rows[0];
+}
+
 module.exports = {
   createUser,
   findUserByUsername,
   findUserById,
+  getAllMessages,
+  createMessage,
 };
